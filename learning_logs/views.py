105452f47b7b23ea76744482c78ce23727bb9637
Ni_Che <nikita3ch@gmail.com>
2022-06-08 16:29:44 +0300

    Підправив головну сторінку

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -28,7 +28,7 @@ def topics(request):
 @login_required()
 def topic(request, topic_id):
     """Показати одну тему та все що там введено"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Пересвідчитись, що тема належить поточному користувачу
     if topic.owner != request.user:
         raise Http404

bdbd54f3316cde574a37400450162fe799af42b0
Ni_Che <nikita3ch@gmail.com>
2022-09-30 20:33:43 +0300

    update topic.html and entry.html

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -16,7 +16,6 @@ class Topic(models.Model):
 class Entry(models.Model):
     """Якась конкретна інформація до цієї теми"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    owner = models.ForeignKey(Topic.owner, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     entry_name = models.CharField(max_length=200)
